5bf572a466ef5a7b092b61851441531c63fdc662
danielprinced <d.danielprince2003@gmail.com>
2024-11-26 15:18:56 +0530

    feat:added user vo

---
+++ src/main/java/vo/User.java
@@ -0,0 +1,37 @@
+package vo;
+
+public class User {
+    private Integer id;
+    private String name;    // Changed to String for name
+    private String password; // Changed to String for password
+
+    // Getter for id
+    public Integer getId() {
+        return id;
+    }
+
+    // Setter for id
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter for password
+    public String getPassword() {
+        return password;
+    }
+
+    // Setter for password
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
